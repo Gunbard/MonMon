fbe884bb9ae9f7711407bd129e89e01064608a59
Gunbard <gunbard@gmail.com>
2026-02-03 10:10:45 -0600

    Add base app

---
+++ monmon/src/main/java/com/honksoft/monmon/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.honksoft.monmon
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.honksoft.monmon.databinding.ActivityFullscreenBinding
+import com.honksoft.monmon.databinding.ActivityMainBinding
+import com.honksoft.monmon.ui.theme.MonMonTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    val binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+  }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+  Text(
+    text = "Hello $name!",
+    modifier = modifier
+  )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+  MonMonTheme {
+    Greeting("Android")
+  }
+}
